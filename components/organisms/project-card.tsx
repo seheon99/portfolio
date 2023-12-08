f5abc2f0c33523bf85a1fd1fce32342c7964cf62
Seheon Yu <developer@seheon.kr>
2023-12-08 20:22:46 +0900

    Refactor project card component

--- components/organisms/project-card.tsx
+++ components/organisms/project-card.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-import ProjectBody from "#components/molecules/project-body";
-import ProjectHeader from "#components/molecules/project-header";
+import ProjectImage from "#components/atoms/project-image";
+import CategoryTags from "#components/molecules/category-tags";
 
 import { Project } from "../../entities/project";
 
@@ -11,13 +11,23 @@ interface ProjectCardProps {
 
 export default function ProjectCard({ project }: ProjectCardProps) {
   return (
-    <article className="flex max-w-xl flex-col items-start justify-between">
-      <ProjectHeader image={project.image} categories={project.categories} />
-      <ProjectBody
-        href={project.href}
-        title={project.title}
-        description={project.description}
-      />
+    <article className="flex flex-col items-start justify-between">
+      <div className="w-full">
+        <ProjectImage image={project.image} />
+        <div className="mt-4" />
+        <CategoryTags categories={project.categories} />
+      </div>
+      <div className="group relative">
+        <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+          <a href={project.href}>
+            <span className="absolute inset-0" />
+            {project.title}
+          </a>
+        </h3>
+        <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
+          {project.description}
+        </p>
+      </div>
     </article>
   );
 }
