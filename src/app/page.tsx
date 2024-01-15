32740dc5df6cb7daf941b8dfbf328f92a5175790
Seheon Yu <developer@seheon.kr>
2024-01-16 00:16:50 +0900

    Remove project category and update about me text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import DrTrashImage from "@/assets/images/projects/drtrash.png";
 import GiftHubImage from "@/assets/images/projects/gifthub.png";
 import HomeTemplate from "@/components/templates/home";
 import { Project } from "@/entities/project";
-import { projectCategories } from "@/entities/project-category";
 
 const headerLinks = [
   { name: "About Me", href: "#aboutme" },
@@ -22,7 +21,7 @@ const headerStats = [
 const aboutMe = [
   "저는 어릴 때부터 컴퓨터에 관심이 많아 자연스럽게 컴퓨터 공학을 전공하게 되었습니다.",
   "학교에서 컴퓨터 공학을 전공하고 42 Seoul과 SW마에스트로에서 프로젝트를 진행하며 개발 실력을 쌓았습니다.",
-  "현재는 프론트엔드 개발자로서의 첫 시작을 위한 멋진 회사를 찾고 있습니다.",
+  "이제 프론트엔드 개발자로서의 첫 시작을 위한 멋진 회사를 찾고 있습니다.",
 ];
 
 const projects: Project[] = [
@@ -33,13 +32,7 @@ const projects: Project[] = [
     description:
       "Dart/Flutter를 기반으로 상태 관리를 위한 Riverpod, 이벤트 로깅을 위한 Firebase Analytics, 다이나믹한 UX를 위한 Rive를 사용했습니다. 또한 Clean Architecture를 기반으로 Command 패턴을 사용했고 이를 통해 코드 품질을 유지하며 이벤트 로깅과 실행 취소 작업을 쉽게 구현할 수 있었습니다.",
     image: GiftHubImage,
-    categories: [
-      projectCategories.mobile,
-      projectCategories.frontend,
-      projectCategories.dart,
-      projectCategories.flutter,
-      projectCategories.riverpod,
-    ],
+    categories: ["mobile", "frontend", "dart", "flutter", "riverpod"],
   },
   {
     id: 2,
@@ -48,13 +41,7 @@ const projects: Project[] = [
     description:
       "Nest.js를 기반으로 RESTful API를 개발했으며, 클라이언트와 인공지능 서버가 통신할 수 있는 Kafka 서버, 클라이언트가 보낸 이미지가 저장되는 스토리지 서버를 만들어 Micro Service Architecture 형태로 운용했습니다.",
     image: DrTrashImage,
-    categories: [
-      projectCategories.mobile,
-      projectCategories.backend,
-      projectCategories.nestjs,
-      projectCategories.typescript,
-      projectCategories.kafka,
-    ],
+    categories: ["mobile", "backend", "nestjs", "typescript", "kafka"],
   },
 ];
 
--- src/entities/project.ts
+++ src/entities/project.ts
@@ -1,12 +1,10 @@
 import { StaticImageData } from "next/image";
 
-import { ProjectCategory } from "@/entities/project-category";
-
 export interface Project {
   id: number;
   title: string;
   href: string;
   description: string;
   image: StaticImageData;
-  categories: ProjectCategory[];
+  categories: string[];
 }
