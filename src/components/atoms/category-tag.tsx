b3968bef20cca2057acdef675b58c2bdf3357850
Seheon Yu <developer@seheon.kr>
2024-01-16 01:23:22 +0900

    Remove ProjectCategory type

--- src/components/atoms/category-tag.tsx
+++ src/components/atoms/category-tag.tsx
@@ -2,19 +2,17 @@ import React from "react";
 
 import Link from "next/link";
 
-import { ProjectCategory } from "@/entities/project-category";
-
 interface CategoryTagProps {
-  category: ProjectCategory;
+  category: string;
 }
 
 export default function CategoryTag({ category }: CategoryTagProps) {
   return (
     <Link
-      href={category.href}
+      href={""}
       className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 text-xs font-medium text-gray-600 hover:bg-gray-100"
     >
-      {category.title}
+      {category}
     </Link>
   );
 }
