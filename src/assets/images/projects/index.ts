929e969c3b2414e62579e4fb014ca8e91694b8be
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2025-10-06 06:04:19 +0000

    eslint: remove unnecessary blank line in index.ts

--- src/assets/images/projects/index.ts
+++ src/assets/images/projects/index.ts
@@ -1,6 +1,5 @@
 import DrTrashIcon from "./drtrash-icon.png";
 import GiftHubIcon from "./gifthub-icon.jpg";
-
 import GiftHubImage from "./gifthub.png";
 
 export { DrTrashIcon, GiftHubIcon, GiftHubImage };
