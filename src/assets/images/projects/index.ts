6159486ed4d964e649cae36f0f225dd4ccda29d7
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2025-10-07 11:10:12 +0000

    feat(projects): add new exhibition projects and update asset icons
    - Added new project entries: miniRT, philosophers, webserv, transcendence, fit, and timetogo
    - Each project includes bilingual (ko/en) exhibition-style briefs and full tech stack metadata
    - Introduced new SVG and PNG icons for each project in `/assets/images/projects`
    - Replaced old GiftHub JPG icon with scalable SVG version
    - Updated project index exports to include all new icons for consistent import handling

--- src/assets/images/projects/index.ts
+++ src/assets/images/projects/index.ts
@@ -1,5 +1,21 @@
 import DrTrashIcon from "./drtrash-icon.png";
-import GiftHubIcon from "./gifthub-icon.jpg";
+import FitIcon from "./fit-icon.svg";
+import GiftHubIcon from "./gifthub-icon.svg";
 import GiftHubImage from "./gifthub.png";
+import MiniRtIcon from "./minirt-icon.png";
+import PhilosophersIcon from "./philosophers-icon.png";
+import TimeToGoIcon from "./timetogo-icon.svg";
+import TranscendenceIcon from "./transcendence-icon.png";
+import WebservIcon from "./webserv-icon.png";
 
-export { DrTrashIcon, GiftHubIcon, GiftHubImage };
+export {
+  DrTrashIcon,
+  FitIcon,
+  GiftHubIcon,
+  MiniRtIcon,
+  GiftHubImage,
+  PhilosophersIcon,
+  TimeToGoIcon,
+  TranscendenceIcon,
+  WebservIcon,
+};
