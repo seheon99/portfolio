1079b5896d45023568c1b4b286398530177e5577
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2025-11-02 20:56:38 +0900

    Refine Google Analytics integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 
-import Script from "next/script";
 import React from "react";
 
 import "@/app/globals.css";
+import { GoogleAnalytics } from "./google-analytics";
 
 export const metadata: Metadata = {
   title: "Seheon Yu Portfolio",
@@ -18,19 +18,7 @@ export default function RootLayout({
   return (
     <html className="relative h-full antialiased" lang="ko">
       <head>
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-YZLPL173WW"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-YZLPL173WW');
-          `}
-        </Script>
+        <GoogleAnalytics />
       </head>
       <body className="overflow-x-clip">{children}</body>
     </html>
