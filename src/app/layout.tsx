35d910edd0b1f0aabc49393dd78fc9ba1b0884f7
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2025-11-02 20:53:45 +0900

    Add Google Analytics tag to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 
+import Script from "next/script";
 import React from "react";
 
 import "@/app/globals.css";
@@ -16,6 +17,21 @@ export default function RootLayout({
 }) {
   return (
     <html className="relative h-full antialiased" lang="ko">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-YZLPL173WW"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-YZLPL173WW');
+          `}
+        </Script>
+      </head>
       <body className="overflow-x-clip">{children}</body>
     </html>
   );
